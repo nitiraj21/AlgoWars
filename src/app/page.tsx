b42ad17bae044ffb6851a2bb860f848c1212560d
nitiraj_21 <nitirajsc21@gmail.com>
2025-07-09 22:40:04 +0530

    added next-auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { get } from "http";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
+import { authOptions } from "../lib/auth";
 
-export default function Home() {
+
+export default async function Home() {
+  const session  = await getServerSession(authOptions);
+  console.log(session?.user?.name);
   return (
-    <div className="flex justify-center items-center h-screen">Code Clash</div>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="grid grid-rows-2 justify-center items-center">
+        <div>Code Clash</div>
+        <div>{session?.user?.name}</div>
+      </div>
+    </div>
   );
 }
