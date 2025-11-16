31a3604669f7565ccad3ad4781e6b1215466774d
nitiraj_21 <nitirajsc21@gmail.com>
2025-11-16 12:42:30 +0530

    redis client

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -4,8 +4,8 @@ export const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-13470.crce179.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 13470
+        host: 'redis-15554.crce182.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 1555
     }
 });
 
